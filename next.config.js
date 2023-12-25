3361d882b67762e0b22a3eff26a0a62bdb24d12f
Guy Korland <gkorland@gmail.com>
2023-12-25 10:46:03 +0200

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    output: "export",
+    images: {
+      unoptimized: true
+    }
+}
 
 module.exports = nextConfig
