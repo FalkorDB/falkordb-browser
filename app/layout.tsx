c38042b121900a5051b888bf664611cdfa75b5d7
Guy Korland <gkorland@gmail.com>
2024-01-21 15:17:07 +0200

    suppressHydrationWarning due to  next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { NextAuthProvider } from './providers'
+import { ThemeProvider } from 'next-themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,10 +16,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  // Setting suppressHydrationWarning on html tag to prevent warning 
+  // caused by mismatched client/server content caused by next-themes
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-          <NextAuthProvider>{children}</NextAuthProvider>
+        <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
   )
