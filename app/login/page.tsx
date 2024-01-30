c89370cfb425d5808a85eb55ef32f4274cc09c6a
Dappmaster (FullstackBlockchain Developer) <60449971+Full-stack-Web-Dev-BD@users.noreply.github.com>
2024-01-30 21:12:58 +0600

    Resolved 2 Issue -Feedback CLosed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,15 +10,20 @@ import { useRouter } from "next/navigation"
 export default function Page() {
     const router = useRouter()
     return (
-        <div className="flex justify-center mt-5" style={{ marginTop: '10%' }}>
+        <div className="flex items-center justify-center h-screen">
             <div
                 className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
             >
                 <form className="flex flex-col gap-2"
                     onSubmit={(e) => {
                         e.preventDefault();
-                        signIn("credentials", { redirect: false });
-                        router.push('/graph')
+                        signIn("credentials", { redirect: false }).then((result: any) => {
+                            if (result.error) {
+                                // Handle error, show message to user
+                            } else {
+                                router.push('/graph');
+                            }
+                        });
                     }}
                 >
                     <div className=" p-4">
