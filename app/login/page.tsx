4119d1489b9cb7d3846f1f98ee3042a1718c99e5
Dappmaster (FullstackBlockchain Developer) <60449971+Full-stack-Web-Dev-BD@users.noreply.github.com>
2024-01-30 19:43:31 +0600

    Added Login screen in App layout with UI improvement -Review Required

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,51 +4,56 @@ import { CardTitle, CardDescription } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { FormEvent } from "react"
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation"
 
 export default function Page() {
-    function handleSubmit(event: FormEvent<HTMLFormElement>): void {
-        event.preventDefault();
-
-        // Read the form data
-        const form = event.target as HTMLFormElement;
-        const formData = new FormData(form);
-        console.log(formData);
-    }
-
+    const router = useRouter()
     return (
-        <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
-            <div className="p-4">
-                <CardTitle className="text-2xl font-semibold">Login FalkorDB server</CardTitle>
-                <CardDescription className="text-gray-500">
-                    Fill in the form below to login to your FalkorDB server.
-                </CardDescription>
+        <div className="flex justify-center mt-5" style={{ marginTop: '10%' }}>
+            <div
+                className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+                <form className="flex flex-col gap-2"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        signIn("credentials", { redirect: false });
+                        router.push('/graph')
+                    }}
+                >
+                    <div className=" p-4">
+                        <CardTitle className="text-2xl font-semibold">Login FalkorDB server</CardTitle>
+                        <CardDescription className="text-gray-500">
+                            Fill in the form below to login to your FalkorDB server.
+                        </CardDescription>
+                    </div>
+                    <div className="space-y-4 flex flex-col p-4">
+                        <div className="grid grid-cols-1 gap-4">
+                            <div className="space-y-2">
+                                <Label htmlFor="server">Server</Label>
+                                <Input id="server" placeholder="localhost" type="text" defaultValue="localhost" />
+                            </div>
+                            <div className="space-y-2">
+                                <Label htmlFor="port">Port</Label>
+                                <Input id="port" placeholder="6379" type="number" min={1} max={65535} defaultValue={6379} />
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 gap-4">
+                            <div className="space-y-2">
+                                <Label htmlFor="username">User Name</Label>
+                                <Input id="username" type="text" />
+                            </div>
+                            <div className="space-y-2">
+                                <Label htmlFor="password">Password</Label>
+                                <Input id="password" type="password" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex justify-center p-4">
+                        <Button type="submit">Connect</Button>
+                    </div>
+                </form>
             </div>
-            <div className="space-y-4 flex flex-col p-4">
-                <div className="grid grid-cols-2 gap-4">
-                    <div className="space-y-2">
-                        <Label htmlFor="server">Server</Label>
-                        <Input id="server" placeholder="localhost" type="text" defaultValue="localhost" />
-                    </div>
-                    <div className="space-y-2">
-                        <Label htmlFor="port">Port</Label>
-                        <Input id="port" placeholder="6379" type="number" min={1} max={65535} defaultValue={6379} />
-                    </div>
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                    <div className="space-y-2">
-                        <Label htmlFor="username">User Name</Label>
-                        <Input id="username" type="text" />
-                    </div>
-                    <div className="space-y-2">
-                        <Label htmlFor="password">Password</Label>
-                        <Input id="password" type="password" />
-                    </div>
-                </div>
-            </div>
-            <div className="flex justify-end p-4">
-                <Button type="submit">Connect</Button>
-            </div>
-        </form>
+        </div>
     )
 }
