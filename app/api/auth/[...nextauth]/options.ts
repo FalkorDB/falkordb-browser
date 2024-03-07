14c65da8bf99e721c6e8351b2c3d5592eab9d3c3
Anchel123 <110421452+Anchel123@users.noreply.github.com>
2024-03-07 10:54:51 +0200

    adding a function that create a client and connect him

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -5,19 +5,47 @@ import { AuthOptions, User } from "next-auth"
 
 const connections = new Map<number, RedisClientType>();
 
+async function client(credentials: {host: string, port: string, password: string, username: string}, id: number) {
+    const client = await createClient({
+        socket: {
+            host: credentials.host ?? "localhost",
+            port: credentials.port ? parseInt(credentials.port) : 6379,
+            reconnectStrategy: false
+        },
+        password: credentials.password ?? undefined,
+        username: credentials.username ?? undefined
+    })
+
+    // Save connection in connections map for later use
+    connections.set(id, client as RedisClientType)
+
+    await client.on('error', err => {
+        // Close coonection on error and remove from connections map
+        console.error('FalkorDB Client Error', err)
+        const connection = connections.get(id)
+        if (connection) {
+            connections.delete(id)
+            connection.disconnect()
+            .catch((e) => {
+                console.warn('FalkorDB Client Disconnect Error', e)
+            })
+        }
+    }).connect();
+
+    // Verify connection
+    await client.ping()
+    return client
+}
+
 export async function getConnection(user: User) {
     let conn = connections.get(user.id)
     if (!conn) {
-        conn = await createClient({
-            socket: {
-                host: user.host ?? "localhost",
-                port: user.port ?? 6379,
-                reconnectStrategy: false
-            },
-            password: user.password ?? undefined,
-            username: user.username ?? undefined
-        })
-        connections.set(user.id, conn)
+        await client({
+            host: user.host,
+            port: user.port.toString() ?? "6379",
+            username: user.username,
+            password: user.password,
+        }, user.id)
     }
     return conn  
 }
@@ -44,34 +72,7 @@ const authOptions: AuthOptions = {
                     const id = userId;
                     userId += 1;
 
-                    const client = await createClient({
-                        socket: {
-                            host: credentials.host ?? "localhost",
-                            port: credentials.port ? parseInt(credentials.port, 10) : 6379,
-                            reconnectStrategy: false
-                        },
-                        password: credentials.password ?? undefined,
-                        username: credentials.username ?? undefined
-                    })
-
-                    // Save connection in connections map for later use
-                    connections.set(id, client as RedisClientType)
-
-                    await client.on('error', err => {
-                        // Close coonection on error and remove from connections map
-                        console.error('FalkorDB Client Error', err)
-                        const connection = connections.get(id)
-                        if (connection) {
-                            connections.delete(id)
-                            connection.disconnect()
-                            .catch((e) => {
-                                console.warn('FalkorDB Client Disconnect Error', e)
-                            })
-                        }
-                    }).connect();
-                
-                    // Verify connection
-                    await client.ping()
+                    await client(credentials, id)
 
                     const res = {
                         id,
