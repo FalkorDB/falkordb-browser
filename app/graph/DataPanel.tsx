fb9ec57bbd0d39a07b265980a0ffb1ff2d3ed4c4
Anchel123 <110421452+Anchel123@users.noreply.github.com>
2024-03-11 12:39:58 +0200

    new format for data view and data panel

--- app/graph/DataPanel.tsx
+++ app/graph/DataPanel.tsx
@@ -1,7 +1,7 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
-export default function DataPanel({node}: {node: Node}) {
+export default function DataPanel({ node }: { node: Node }) {
     return (
         <Table>
             <TableHeader>
@@ -12,28 +12,43 @@ export default function DataPanel({node}: {node: Node}) {
             </TableHeader>
             <TableBody>
                 {
-                    Object.entries(node).map((row, index) => (
-                        // eslint-disable-next-line react/no-array-index-key
-                        <TableRow key={index}>
-                            {
-                                Object.values(row).map((cell, cellIndex) => (
-                                    // eslint-disable-next-line react/no-array-index-key
-                                    <TableCell key={cellIndex}>
-                                        <TooltipProvider>
-                                            <Tooltip>
-                                                <TooltipTrigger className="max-w-96 truncate">
-                                                    {JSON.stringify(cell)}
-                                                </TooltipTrigger>
-                                                <TooltipContent>
-                                                    <p>{JSON.stringify(cell)}</p>
-                                                </TooltipContent>
-                                            </Tooltip>
-                                        </TooltipProvider>
-                                    </TableCell>
-                                ))
-                            }
-                        </TableRow>
-                    ))
+                    Object.entries(node).map((row, index) => {
+                        let i = 0;
+                        return (
+                            // eslint-disable-next-line react/no-array-index-key
+                            <TableRow key={index}>
+                                {
+                                    Object.values(row).map((cell, cellIndex) => {
+                                        debugger
+                                        let text = "";
+                                        if (i == 0) {
+                                            text = JSON.stringify(cell)
+                                            i++
+                                        } else {
+                                            text = JSON.stringify(cell).replace(/"/g, '')
+                                            i--
+                                        }
+
+                                        return (
+                                            // eslint-disable-next-line react/no-array-index-key
+                                            <TableCell key={cellIndex}>
+                                                <TooltipProvider>
+                                                    <Tooltip>
+                                                        <TooltipTrigger className="max-w-96 truncate">
+                                                            {text}
+                                                        </TooltipTrigger>
+                                                        <TooltipContent>
+                                                            <p>{text}</p>
+                                                        </TooltipContent>
+                                                    </Tooltip>
+                                                </TooltipProvider>
+                                            </TableCell>
+                                        )
+                                    })
+                                }
+                            </TableRow>
+                        )
+                    })
                 }
             </TableBody>
         </Table>
--- app/graph/tableview.tsx
+++ app/graph/tableview.tsx
@@ -4,16 +4,17 @@ import { Graph } from "./model";
 
 // eslint-disable-next-line import/prefer-default-export
 export function TableView({ graph }: { graph: Graph }) {
+    console.log(Object.values(graph.Data[0]).map((cell) => cell));
     return (
         <Table>
             <TableCaption>A list of results</TableCaption>
             <TableHeader>
                 <TableRow>
                     {
-                        graph.Columns.map((column, index) => (
+                        graph.Columns.map((column, index) => {
                             // eslint-disable-next-line react/no-array-index-key
-                            <TableHead key={index}>{column}</TableHead>
-                        ))
+                            return <TableHead key={index}>{column}</TableHead>
+                        })
                     }
                 </TableRow>
             </TableHeader>
@@ -23,15 +24,40 @@ export function TableView({ graph }: { graph: Graph }) {
                         // eslint-disable-next-line react/no-array-index-key
                         <TableRow key={index}>
                             {
-                                Object.values(row).map((cell, cellIndex) => (
-                                    // eslint-disable-next-line react/no-array-index-key
-                                    <TableCell key={cellIndex}>
-                                        <TooltipProvider><Tooltip><TooltipTrigger className="max-w-96 truncate">
-                                            {JSON.stringify(cell)}
-                                        </TooltipTrigger><TooltipContent><p>{JSON.stringify(cell)}</p></TooltipContent></Tooltip></TooltipProvider>
-                                    </TableCell>
-                                ))
+                                Object.values(row).map((cell, cellIndex) => {
+                                    const text = JSON.stringify(cell)
+                                        .replace(/[{}\[\]":]/g, (match) => {
+                                            switch (match) {
+                                                case '{':
+                                                case '}':
+                                                case '[':
+                                                case ']':
+                                                case '"':
+                                                    return '';
+                                                case ':':
+                                                    return ': ';
+                                                case ',':
+                                                    return ', ';
+                                            }
+                                        })
+                                    return (
+                                        // eslint-disable-next-line react/no-array-index-key
+                                        <TableCell key={cellIndex}>
+                                            <TooltipProvider>
+                                                <Tooltip>
+                                                    <TooltipTrigger className="max-w-96 truncate">
+                                                        {text}
+                                                    </TooltipTrigger>
+                                                    <TooltipContent>
+                                                        <p>{text}</p>
+                                                    </TooltipContent>
+                                                </Tooltip>
+                                            </TooltipProvider>
+                                        </TableCell>
+                                    )
+                                })
                             }
+
                         </TableRow>
                     ))
                 }
