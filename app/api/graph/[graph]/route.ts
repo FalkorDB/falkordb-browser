72e32e38a1b8dfc7af183a954228e307fb59fcc2
Anchel123 <110421452+Anchel123@users.noreply.github.com>
2024-03-27 16:02:30 +0200

    add menu and delete

---
+++ app/api/graph/[graph]/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import authOptions, { getConnection } from "../../auth/[...nextauth]/options";
+
+// eslint-disable-next-line import/prefer-default-export
+export async function DELETE(request: NextRequest, { params }: { params: { graph: string } }) {
+
+    const session = await getServerSession(authOptions)
+    const id = session?.user?.id
+    if (!id) {
+        return NextResponse.json({ message: "Not authenticated" }, { status: 401 })
+    }
+
+    const client = await getConnection(session.user)
+    if (!client) {
+        return NextResponse.json({ message: "Not authenticated" }, { status: 401 })
+    }
+
+    const graphId = params.graph;
+
+    try {
+        if (graphId) {
+            
+            const graph = client.selectGraph(graphId);
+
+            const del = graph.delete()
+            
+            return NextResponse.json({ success: del }, { status: 200 })
+        }
+    } catch (err: unknown) {
+        return NextResponse.json({ message: (err as Error).message }, { status: 400 })
+    }
+}
--- app/graph/query.tsx
+++ app/graph/query.tsx
@@ -2,8 +2,13 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GraphsList from "./GraphList";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem, DropdownMenuGroup } from "@radix-ui/react-dropdown-menu";
+import { toast } from "@/components/ui/use-toast";
+import { Menu, Trash2 } from "lucide-react";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogTitle, AlertDialogTrigger } from "@radix-ui/react-alert-dialog";
+import { AlertDialogHeader, AlertDialogFooter } from "@/components/ui/alert-dialog";
 
 
 export class QueryState {
@@ -23,6 +28,20 @@ export function Query({ onSubmit, onQueryUpdate, className = "" }: {
 
     onQueryUpdate(new QueryState(query, graphName))
 
+    const handelDelete = async () => {
+        const result = await fetch(`/api/graph/${encodeURIComponent(graphName)}`, {
+            method: 'DELETE',
+        })
+        console.log(result.status);
+        if (result.status >= 300) {
+            return
+        }
+        toast({
+            title: "Delete",
+            description: "Graph deleted successfully"
+        })
+    }
+
     return (
         <form
             className={cn("flex flex-col space-y-3 md:flex-row md:space-x-3 md:space-y-0", className)}
@@ -31,10 +50,22 @@ export function Query({ onSubmit, onQueryUpdate, className = "" }: {
                 <Label htmlFor="query" className="text">Query</Label>
                 <GraphsList onSelectedGraph={setGraphName} />
             </div>
-            <div className="flex flex-row space-x-3 w-full md:w-8/12">
+            <div className="flex flex-row space-x-3 w-full md:w-8/12 items-center">
                 <Input id="query" className="border-gray-500 w-full"
                     placeholder="MATCH (n)-[e]-() RETURN n,e limit 100" type="text" onChange={(event) => setQuery(event.target.value)} />
-                <Button type="submit">Run</Button>
+                <Button type="submit" className="mr-16">Run</Button>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button><Menu/></Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-32 flex flex-col p-4 items-center mt-2 gap-y-2 rounded-lg bg-black border-white border">
+                            <DropdownMenuItem onClick={() => handelDelete()} className="flex flex-row gap-x-2">
+                                <Trash2/>
+                                <span>delete</span>
+                            </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
             </div>
         </form>
     )
