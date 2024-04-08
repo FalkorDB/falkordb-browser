195371e351347b2de1dd92b9b13a7b8970cf3a84
Anchel123 <110421452+Anchel123@users.noreply.github.com>
2024-04-08 16:38:23 +0300

    fix tests

--- e2e/homePage.spec.ts
+++ e2e/homePage.spec.ts
@@ -6,13 +6,13 @@ test.beforeEach(async ({ page }) => {
 
 test('connect', async ({ page }) => {
     await page.getByRole('button').click();
-    await page.waitForURL('http://localhost:3000');
-    expect(page.url()).toBe('http://localhost:3000');
+    await page.waitForURL('http://localhost:3000/login');
+    expect(page.url()).toBe('http://localhost:3000/login');
   
 });
 
 test('themes', async ({ page }) => {
   await page.getByLabel('system mode').click();
   await page.getByLabel('dark mode').click();
-  expect(page.getByLabel('light mode')).toBeVisible();
+  await page.getByLabel('light mode').click();
 });
