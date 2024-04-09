2ae224b30d72db03834a78fa6accb459e826ebcc
Anchel123 <110421452+Anchel123@users.noreply.github.com>
2024-04-09 11:25:38 +0300

    fix lint

--- components/custom/spinning.tsx
+++ components/custom/spinning.tsx
@@ -1,6 +1,7 @@
 import { Skeleton } from "@/components/ui/skeleton"
 
-export function Spinning() {
+
+export default function Spinning() {
   return (
     <div className="flex items-center space-x-4">
       <Skeleton className="h-12 w-12 rounded-full" />
