0db4c92f5c6f4a35016d6e69f9619acb75ce3656
Anchel123 <110421452+Anchel123@users.noreply.github.com>
2024-04-09 13:56:15 +0300

    space format

--- e2e/homePage.spec.ts
+++ e2e/homePage.spec.ts
@@ -1,17 +1,17 @@
 import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+    await page.goto('http://localhost:3000/');
 });
 
 test('connect', async ({ page }) => {
-  await page.getByRole('button').click();
-  await page.waitForURL('http://localhost:3000/login');
-  expect(page.url()).toBe('http://localhost:3000/login');
+    await page.getByRole('button').click();
+    await page.waitForURL('http://localhost:3000/login');
+    expect(page.url()).toBe('http://localhost:3000/login');
 });
 
 test('themes', async ({ page }) => {
-  await page.getByLabel('system mode').click();
-  await page.getByLabel('dark mode').click();
-  await page.getByLabel('light mode').click();
+    await page.getByLabel('system mode').click();
+    await page.getByLabel('dark mode').click();
+    await page.getByLabel('light mode').click();
 });
