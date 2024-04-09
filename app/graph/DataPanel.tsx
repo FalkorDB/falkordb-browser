44fe5e75079de3e809eb36967c8e4286f90f0aab
Anchel123 <110421452+Anchel123@users.noreply.github.com>
2024-04-09 13:23:59 +0300

    fix

--- app/graph/DataPanel.tsx
+++ app/graph/DataPanel.tsx
@@ -1,13 +1,14 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
-const undisplayedProperties = [
+const excludedProperties = new Set([
     "category",
     "color",
     "label",
-    "source",
-    "target"
-]
+    "target",
+    "source"
+]);
+
 export default function DataPanel({ node }: { node: Node }) {
     const rowClass = "dark:hover:bg-slate-700 hover:bg-gray-400 border-y-[1px] border-y-gray-700"
 
@@ -21,33 +22,33 @@ export default function DataPanel({ node }: { node: Node }) {
             </TableHeader>
             <TableBody>
                 {
-                    Object.entries(node).filter((row) => undisplayedProperties.find(category => row[0] === category) === undefined).map((row, index) => (
-                            // eslint-disable-next-line react/no-array-index-key
-                            <TableRow className={rowClass} key={index}>
-                                {
-                                    Object.values(row).map((cell, cellIndex) => {
+                    Object.entries(node).filter((row) => !excludedProperties.has(row[0])).map((row, index) => (
+                        // eslint-disable-next-line react/no-array-index-key
+                        <TableRow className={rowClass} key={index}>
+                            {
+                                Object.values(row).map((cell, cellIndex) => {
 
-                                        const strCell = JSON.stringify(cell)
-                                        const text = cellIndex === 1 ? JSON.parse(strCell) : strCell
-                                        return (
-                                            // eslint-disable-next-line react/no-array-index-key
-                                            <TableCell key={cellIndex}>
-                                                <TooltipProvider>
-                                                    <Tooltip>
-                                                        <TooltipTrigger className="max-w-96 truncate">
-                                                            {text}
-                                                        </TooltipTrigger>
-                                                        <TooltipContent>
-                                                            <p>{text}</p>
-                                                        </TooltipContent>
-                                                    </Tooltip>
-                                                </TooltipProvider>
-                                            </TableCell>
-                                        )
-                                    })
-                                }
-                            </TableRow>
-                        ))
+                                    const strCell = JSON.stringify(cell)
+                                    const text = cellIndex === 1 ? JSON.parse(strCell) : strCell
+                                    return (
+                                        // eslint-disable-next-line react/no-array-index-key
+                                        <TableCell key={cellIndex}>
+                                            <TooltipProvider>
+                                                <Tooltip>
+                                                    <TooltipTrigger className="max-w-96 truncate">
+                                                        {text}
+                                                    </TooltipTrigger>
+                                                    <TooltipContent>
+                                                        <p>{text}</p>
+                                                    </TooltipContent>
+                                                </Tooltip>
+                                            </TooltipProvider>
+                                        </TableCell>
+                                    )
+                                })
+                            }
+                        </TableRow>
+                    ))
                 }
             </TableBody>
         </Table>
