68d0b6e4673b85a8250084c9d971d3e6eaf6ba56
Anchel123 <110421452+Anchel123@users.noreply.github.com>
2024-04-10 14:18:44 +0300

    use join

--- app/graph/metadataview.tsx
+++ app/graph/metadataview.tsx
@@ -1,10 +1,7 @@
 export default function MetaDataView({ metadata }: { metadata: string[] }) {
     return (
         <p>
-            {
-                // eslint-disable-next-line react/no-array-index-key
-                metadata.map((val, index) => index === 0 ? val : ", ".concat(val)  )
-            }
+            {metadata.join(", ")}
         </p>
     )
 }
