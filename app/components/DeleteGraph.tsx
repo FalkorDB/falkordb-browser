45f71842b404104bbc6f680b8d31cdc8cdf0bf94
Anchel135 <yakoveidelman1@gmail.com>
2024-07-11 12:54:03 +0300

    update the state of the graphs array and the selected graph on delete

--- app/components/DeleteGraph.tsx
+++ app/components/DeleteGraph.tsx
@@ -1,10 +1,11 @@
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
 import { Toast, securedFetch } from "@/lib/utils";
 
-export default function DeleteGraph({ graphName, isOpen, onOpen }: {
+export default function DeleteGraph({ graphName, isOpen, onOpen, onDeleteGraph }: {
     graphName: string
     isOpen: boolean
     onOpen: (open: boolean) => void
+    onDeleteGraph: () => void
 }) {
 
     const deleteGraph = async () => {
@@ -13,18 +14,21 @@ export default function DeleteGraph({ graphName, isOpen, onOpen }: {
         });
 
         if (result.ok) {
-            Toast("Success", "Graph deleted")
+            Toast(`Graph ${graphName} deleted`, "Success")
+            onDeleteGraph()
         }
     }
 
     return (
         <AlertDialog open={isOpen} onOpenChange={onOpen}>
             <AlertDialogContent className="flex flex-col gap-10 p-4">
-                <AlertDialogHeader className="">
+                <AlertDialogHeader>
                     <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
                 </AlertDialogHeader>
-                <AlertDialogDescription>
-                    This action cannot be undone. This will permanently delete the selected graph.
+                <AlertDialogDescription className="text-xl">
+                    This action cannot be undone !
+                    <br />
+                    This will permanently delete the {graphName} graph.
                 </AlertDialogDescription>
                 <AlertDialogFooter>
                     <AlertDialogCancel>Cancel</AlertDialogCancel>
