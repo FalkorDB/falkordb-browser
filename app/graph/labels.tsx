e0bf3e400b397696e3038e40e1fddbb9bcb60b84
Anchel135 <yakoveidelman1@gmail.com>
2024-09-02 13:28:27 +0300

    fix the overlay in the labels

--- app/graph/labels.tsx
+++ app/graph/labels.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { cn } from "@/lib/utils";
 import { Category, getCategoryColorName } from "../api/graph/model";
 import Button from "../components/ui/Button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 /* eslint-disable react/require-default-props */
 interface Props {
@@ -15,32 +16,54 @@ export default function Labels({ categories, onClick, label, className = "" }: P
 
     // fake state to force reload
     const [, setReload] = useState(false)
+    const listRef = useRef<HTMLUListElement>(null)
+
+    const handelScroll = (scrollTo: number) => {
+        listRef.current?.scrollBy({
+            behavior: "smooth",
+            left: scrollTo,
+        })
+    }
 
     return (
-        <div className={cn(className, "absolute bottom-0 flex flex-col gap-2 p-4")}>
+        <div className={cn(className, "absolute bottom-0 flex flex-col gap-2 p-4 w-1/2")}>
             {
                 label &&
                 <h1>{label}</h1>
             }
-            <ul className="flex gap-6" >
-                {
-                    categories.map((category) => (
-                        <li key={category.index}>
-                            <Button
-                                className={cn(category.name && "flex gap-2 items-center")}
-                                label={category.name}
-                                icon={
-                                    <div className={cn("w-6 h-6 rounded-full", `bg-${getCategoryColorName(category.index)}`, label === "RelationshipTypes" && "opacity-50")} />
-                                }
-                                onClick={() => {
-                                    onClick(category)
-                                    setReload(prev => !prev)
-                                }}
-                            />
-                        </li>
-                    ))
-                }
-            </ul>
+            <div className={cn("flex items-center gap-4", label === "RelationshipTypes" && "flex-row-reverse")}>
+                <div className="flex flex-col">
+                    <Button
+                        icon={<ChevronRight />}
+                        title="Scroll right"
+                        onClick={() => handelScroll(100)}
+                    />
+                    <Button
+                        icon={<ChevronLeft />}
+                        title="Scroll left"
+                        onClick={() => handelScroll(-100)}
+                    />
+                </div>
+                <ul ref={listRef} className={cn("flex gap-6 w-full overflow-auto hide-scrollbar items-center", label === "RelationshipTypes" && "flex-row-reverse")}>
+                    {
+                        categories.map((category) => (
+                            <li key={category.index}>
+                                <Button
+                                    className={cn(category.name && "flex gap-2 items-center")}
+                                    label={category.name}
+                                    icon={
+                                        <div className={cn("w-6 h-6 rounded-full", `bg-${getCategoryColorName(category.index)}`, label === "RelationshipTypes" && "opacity-50")} />
+                                    }
+                                    onClick={() => {
+                                        onClick(category)
+                                        setReload(prev => !prev)
+                                    }}
+                                />
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
