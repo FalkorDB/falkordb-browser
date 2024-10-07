fb8f57f09e3872cd731359b8ad54e7a2c9171454
Anchel135 <yakoveidelman1@gmail.com>
2024-10-07 13:25:40 +0300

    add to read write the delete and to both the info

--- app/api/user/options.ts
+++ app/api/user/options.ts
@@ -7,7 +7,7 @@ export interface CreateUser {
 export const ROLE = new Map<string, string[]>(
     [
         ["Admin", ["on", "~*", "&*", "+@all"]],
-        ["Read-Write", ["on", "~*", "resetchannels", "-@all", "+graph.query", "+graph.ro_query", "+graph.explain", "+graph.list", "+ping", "+graph.profile", "+info"]],
-        ["Read-Only", ["on", "~*", "resetchannels", "-@all", "+graph.ro_query", "+graph.explain", "+graph.list", "+ping", "+info"]]
+        ["Read-Write", ["on", "~*", "resetchannels", "-@all", "+graph.explain", "+graph.list", "+ping", "+graph.ro_query", "+info", "+graph.delete", "+graph.query", "+graph.profile"]],
+        ["Read-Only", ["on", "~*", "resetchannels", "-@all", "+graph.explain", "+graph.list", "+ping", "+graph.ro_query", "+info"]]
     ]
 )
