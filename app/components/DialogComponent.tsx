14ad9bf6174aceed2f53e3e045d77d72407607fe
Anchel135 <yakoveidelman1@gmail.com>
2025-01-30 12:31:42 +0200

    commit

--- app/components/DialogComponent.tsx
+++ app/components/DialogComponent.tsx
@@ -33,7 +33,7 @@ export default function DialogComponent({
             </DialogTrigger>
             <DialogContent className={cn("bg-foreground p-8 flex flex-col gap-8 rounded-lg border-none", className)} disableClose>
                 <DialogHeader className="flex-row justify-between items-center border-b border-secondary pb-4">
-                    <DialogTitle className="text-2xl font-medium">{title}</DialogTitle>
+                    <DialogTitle className="Title text-2xl font-medium">{title}</DialogTitle>
                     <CloseDialog />
                 </DialogHeader>
                 {
