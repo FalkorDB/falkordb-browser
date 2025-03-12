acb531de631f4b72abcec9df428f05d26926b820
Anchel135 <yakoveidelman1@gmail.com>
2025-03-12 13:02:59 +0200

    remove log

--- app/graph/page.tsx
+++ app/graph/page.tsx
@@ -76,7 +76,6 @@ export default function Page() {
         const json = await result.json()
         fetchCount()
         setSelectedElement(undefined)
-        console.log(json.result.data);
         
         return json.result
     }
