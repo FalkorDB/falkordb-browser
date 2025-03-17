1e1df7ae35ce80a0f95a4abacad342a34af963d1
Anchel135 <yakoveidelman1@gmail.com>
2025-03-17 10:06:15 +0200

    Refactor MetadataView layout and improve styling
    This commit reorganizes the MetadataView component to enhance the layout and visual hierarchy. Key changes include:
    - Swapped the order of the Profile, Metadata, and Explain sections for better usability.
    - Updated button sizes and styles for improved accessibility.
    - Adjusted JSONTree theme colors for better contrast and readability.
    - Ensured consistent padding and borders across sections for a cleaner look.

--- app/graph/MetadataView.tsx
+++ app/graph/MetadataView.tsx
@@ -19,62 +19,35 @@ export default function MetadataView({ query, graphName }: {
     }
 
     return (
-        <div className="flex flex-col gap-4">
-            <h1>Metadata</h1>
-            {query.metadata.map((m, i) => (
-                // eslint-disable-next-line react/no-array-index-key
-                <pre key={i}>{m}</pre>
-            ))}
-            <h1>Explain</h1>
-            <JSONTree
-                data={createNestedObject(query.explain)}
-                shouldExpandNodeInitially={() => true}
-                hideRoot
-                theme={{
-                    base00: "var(--background)", // background
-                    base01: '#000000',
-                    base02: '#CE9178',
-                    base03: '#CE9178', // open values
-                    base04: '#CE9178',
-                    base05: '#CE9178',
-                    base06: '#CE9178',
-                    base07: '#CE9178',
-                    base08: '#CE9178',
-                    base09: '#b5cea8', // numbers
-                    base0A: '#CE9178',
-                    base0B: '#CE9178', // close values
-                    base0C: '#CE9178',
-                    base0D: '#99E4E5', // * keys
-                    base0E: '#ae81ff',
-                    base0F: '#cc6633'
-                }}
-            />
-            <div className="flex gap-2">
-                <Button
-                    variant="Primary"
-                    label="Profile"
-                    onClick={handleProfile}
-                />
-                <Button
-                    className="cursor-default"
-                    title="be aware that running the explain command will change you'r data"
-                >
-                    <Info size={20} />
-                </Button>
-            </div>
-            {
-                profile.length > 0 &&
-                <>
-                    <h1>Profile</h1>
+        <div className="h-full flex">
+            <div className="grow flex flex-col gap-4 border p-8">
+                <h1 className="text-2xl font-bold p-2">Profile</h1>
+                <div className="flex gap-4">
+                    <Button
+                        variant="Primary"
+                        label="Profile"
+                        onClick={handleProfile}
+                    />
+                    <Button
+                        className="cursor-default"
+                        title="be aware that running the profile command will change your data"
+                    >
+                        <Info size={25} />
+                    </Button>
+                </div>
+                {
+                    profile.length > 0 &&
                     <JSONTree
                         data={createNestedObject(profile)}
                         shouldExpandNodeInitially={() => true}
                         hideRoot
+                        // eslint-disable-next-line react/no-unstable-nested-components
+                        labelRenderer={(label) => <span>{label}</span>}
                         theme={{
                             base00: "var(--background)", // background
                             base01: '#000000',
                             base02: '#CE9178',
-                            base03: '#CE9178', // open values
+                            base03: '#242424', // open values
                             base04: '#CE9178',
                             base05: '#CE9178',
                             base06: '#CE9178',
@@ -82,15 +55,53 @@ export default function MetadataView({ query, graphName }: {
                             base08: '#CE9178',
                             base09: '#b5cea8', // numbers
                             base0A: '#CE9178',
-                            base0B: '#CE9178', // close values
+                            base0B: '#242424',
                             base0C: '#CE9178',
-                            base0D: '#99E4E5', // * keys
+                            base0D: '#FFFFFF', // * keys
                             base0E: '#ae81ff',
                             base0F: '#cc6633'
                         }}
                     />
-                </>
-            }
+                }
+
+            </div>
+            <div className="flex flex-col">
+                <div className="grow border p-8">
+                    <h1 className="text-2xl font-bold p-2">Metadata</h1>
+                    {query.metadata.map((m, i) => (
+                        // eslint-disable-next-line react/no-array-index-key
+                        <pre key={i}>{m}</pre>
+                    ))}
+                </div>
+                <div className="grow border p-8">
+                    <h1 className="text-2xl font-bold p-2">Explain</h1>
+                    <JSONTree
+                        data={createNestedObject(query.explain)}
+                        shouldExpandNodeInitially={() => true}
+                        hideRoot
+                        // eslint-disable-next-line react/no-unstable-nested-components
+                        labelRenderer={(label) => <span>{label}</span>}
+                        theme={{
+                            base00: "var(--background)", // background
+                            base01: '#000000',
+                            base02: '#CE9178',
+                            base03: '#242424', // open values
+                            base04: '#CE9178',
+                            base05: '#CE9178',
+                            base06: '#CE9178',
+                            base07: '#CE9178',
+                            base08: '#CE9178',
+                            base09: '#b5cea8', // numbers
+                            base0A: '#CE9178',
+                            base0B: '#242424',
+                            base0C: '#CE9178',
+                            base0D: '#FFFFFF', // * keys
+                            base0E: '#ae81ff',
+                            base0F: '#cc6633'
+                        }}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
