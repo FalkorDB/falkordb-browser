0869123ffe94790e99b88ec728cefe501c64f4ad
Anchel135 <yakoveidelman1@gmail.com>
2025-04-08 14:37:42 +0300

    delete removed attribute instead of giving undefined value on key

--- app/graph/GraphDataPanel.tsx
+++ app/graph/GraphDataPanel.tsx
@@ -151,7 +151,8 @@ export default function GraphDataPanel({ obj, setObj, onExpand, onDeleteElement,
                 const value = obj.data[key]
 
                 graph.removeProperty(key, id)
-                setObj({ ...obj, data: { ...obj.data, [key]: undefined } })
+                delete obj.data[key]
+                setObj({ ...obj })
 
                 toast({
                     title: "Success",
