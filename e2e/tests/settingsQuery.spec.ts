7c603ca660a9985ff78273a63e7f78a9ec3ebc9c
Anchel135 <yakoveidelman1@gmail.com>
2025-04-28 13:03:04 +0300

    Implement new button interactions and enhance settings management in QuerySettings component. Add methods for decreasing limits and timeouts, and update tests to validate behavior for negative values and button interactions.

--- e2e/tests/settingsQuery.spec.ts
+++ e2e/tests/settingsQuery.spec.ts
@@ -24,7 +24,7 @@ test.describe("Query Settings", () => {
         await apiCall.addGraph(graphName)
         const querySettings = await browser.createNewPage(QuerySettingsPage, urls.settingsUrl);
         const timeout = 1;
-        await querySettings.addTimeout(timeout);
+        await querySettings.fillTimeout(timeout);
         await querySettings.clickOnGraph();
         await querySettings.selectExistingGraph(graphName)
         const query = `UNWIND range(1, 100000000) AS x RETURN count(x)`;
@@ -40,7 +40,7 @@ test.describe("Query Settings", () => {
         await apiCall.addGraph(graphName)
         const querySettings = await browser.createNewPage(QuerySettingsPage, urls.settingsUrl);
         const limit = 5;
-        await querySettings.addLimit(limit);
+        await querySettings.fillLimit(limit);
         await querySettings.clickOnGraph();
         await querySettings.selectExistingGraph(graphName);
         const query = `UNWIND range(1, 10) AS i CREATE (p:Person {id: i, name: 'Person ' + toString(i)}) RETURN p`;
@@ -48,6 +48,51 @@ test.describe("Query Settings", () => {
         await querySettings.clickRunQuery()
         const res = await querySettings.getNodeScreenPositions('graph');
         expect(res.length).toBe(5);
+        await querySettings.clickOnSettings();
         await apiCall.removeGraph(graphName);
+    });
+
+    test(`@admin Validate that limit can't be negative with the input`, async () => {
+        const querySettings = await browser.createNewPage(QuerySettingsPage, urls.settingsUrl);
+        const limit = -1;
+        await querySettings.fillLimit(limit);
+        const limitValue = await querySettings.getLimit();
+        expect(limitValue).toBe("∞");
+    });
+
+    test(`@admin Validate that timeout can't be negative with the input`, async () => {
+        const querySettings = await browser.createNewPage(QuerySettingsPage, urls.settingsUrl);
+        const timeout = -1;
+        await querySettings.fillTimeout(timeout);
+        const timeoutValue = await querySettings.getTimeout();
+        expect(timeoutValue).toBe("∞");
+    });
+
+    test(`@admin Validate that limit can't be negative with the decrease button`, async () => {
+        const querySettings = await browser.createNewPage(QuerySettingsPage, urls.settingsUrl);
+        await querySettings.clickDecreaseLimit();
+        const limitValue = await querySettings.getLimit();
+        expect(limitValue).toBe("∞");
+    }); 
+
+    test(`@admin Validate that timeout can't be negative with the decrease button`, async () => {
+        const querySettings = await browser.createNewPage(QuerySettingsPage, urls.settingsUrl);
+        await querySettings.clickDecreaseTimeout();
+        const timeoutValue = await querySettings.getTimeout();
+        expect(timeoutValue).toBe("∞");
+    }); 
+
+    test(`@admin Validate that limit changed with the increase button`, async () => {
+        const querySettings = await browser.createNewPage(QuerySettingsPage, urls.settingsUrl);
+        await querySettings.clickIncreaseLimit();
+        const limitValue = await querySettings.getLimit();
+        expect(limitValue).toBe("1");
+    });
+
+    test(`@admin Validate that timeout changed with the increase button`, async () => {
+        const querySettings = await browser.createNewPage(QuerySettingsPage, urls.settingsUrl);
+        await querySettings.clickIncreaseTimeout();
+        const timeoutValue = await querySettings.getTimeout();
+        expect(timeoutValue).toBe("1");
     });
 });
