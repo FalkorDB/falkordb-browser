8b82869da885597cc529d45fb7cb2b1b8ab682be
Anchel135 <yakoveidelman1@gmail.com>
2025-05-15 12:41:16 +0300

    Refactor graph components to improve layout and functionality
    - Added tabsValue prop to Controls and GraphDetails components for better state management.
    - Enhanced layout in GraphView and SchemaView for improved responsiveness and user experience.
    - Streamlined the rendering of graph information in GraphDetails for consistency and clarity.

--- app/graph/GraphDetails.tsx
+++ app/graph/GraphDetails.tsx
@@ -1,52 +1,44 @@
-import { useContext } from "react"
-import { GraphContext } from "../components/provider"
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
+import { cn } from "@/lib/utils"
+import { Graph } from "../api/graph/model"
 
 export default function GraphDetails({
+    graph,
     tabsValue = "Graph",
     nodesCount,
     edgesCount
 }: {
+    graph: Graph,
     nodesCount: number,
     edgesCount: number,
     tabsValue?: string,
 }) {
-    const { graph } = useContext(GraphContext)
-
     return (
-        <div className="flex gap-4">
+        <div className="w-1 grow flex gap-4 overflow-hidden">
             {
                 graph.Id && tabsValue === "Graph" &&
-                <>
-                    <p>
-                        GraphName:&nbsp;
-                        <span
-                            data-testid="graphName"
-                            className="Gradient bg-clip-text text-transparent"
-                        >
-                            {graph.Id}
-                        </span>
-                    </p>
-                    <p>
-                        Nodes:&nbsp;
-                        <span
-                            data-testid="nodesCount"
-                            className="Gradient bg-clip-text text-transparent"
-                        >
-                            {nodesCount}
-                        </span>
-                    </p>
-                    <p>
-                        Edges:&nbsp;
-                        <span
-                            data-testid="edgesCount"
-                            className="Gradient bg-clip-text text-transparent"
-                        >
-                            {edgesCount}
-                        </span>
-                    </p>
-                </>
+                [["Nodes", nodesCount, "nodesCount"], ["Edges", edgesCount, "edgesCount"], ["GraphName", graph.Id, "graphName"]].map(([label, value, testId]) => (
+                    <div className={cn("flex gap-2 overflow-hidden", label === "GraphName" ? "w-1 grow" : "max-w-[33%]")} key={label}>
+                        <p>
+                            {label}:
+                        </p>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <p
+                                    data-testid={testId}
+                                    className="Gradient bg-clip-text text-transparent truncate pointer-events-auto"
+                                >
+                                    {value}
+                                </p>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                {value}
+                            </TooltipContent>
+                        </Tooltip>
+                    </div>
+                ))
             }
-        </div>
+        </div >
     )
 }
 
