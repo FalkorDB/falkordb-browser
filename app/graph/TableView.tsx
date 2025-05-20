fbe7f603e1589ce80e192b9cd9b698a074a18729
Anchel135 <yakoveidelman1@gmail.com>
2025-05-20 13:12:02 +0300

    Refactor TableView component to utilize useMemo for optimized data handling and improve readability. Added conditional rendering for empty graph data.

--- app/graph/TableView.tsx
+++ app/graph/TableView.tsx
@@ -1,25 +1,36 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { useContext } from "react"
+import { useContext, useMemo } from "react"
+import { Row } from "@/lib/utils";
 import TableComponent from "../components/TableComponent"
 import { GraphContext } from "../components/provider"
 
 export default function TableView() {
     const { graph } = useContext(GraphContext)
-    if (graph.Data.length === 0) return undefined
+
+    const tableData = useMemo(() => {
+        if (graph.Data.length === 0) return undefined;
+        
+        return {
+            headers: Object.keys(graph.Data[0]),
+            rows: graph.Data.map((row): Row => ({
+                cells: Object.values(row).map((value) => ({
+                    value,
+                    type: "object"
+                }))
+            }))
+        };
+    }, [graph.Data]);
+
+    if (tableData === undefined) return undefined
 
     return (
         <TableComponent
             label="TableView"
             className="p-12"
-            headers={Object.keys(graph.Data[0])}
-            rows={graph.Data.map(row => ({
-                cells: Object.values(row).map((value) => ({
-                    value,
-                    type: "object"
-                }))
-            }))}
+            headers={tableData.headers}
+            rows={tableData.rows}
         />
     )
 }
