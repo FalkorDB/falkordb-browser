ae261fef74ebb6bb438e1a6bbd91e9dca0f0e16b
Anchel135 <yakoveidelman1@gmail.com>
2025-07-15 12:08:44 +0300

    Refactor default tab selection logic in GraphView component

--- app/graph/GraphView.tsx
+++ app/graph/GraphView.tsx
@@ -90,9 +90,9 @@ function GraphView({
     useEffect(() => {
         if (isTabEnabled(tabsValue)) return
 
-        let defaultChecked: Tab = "Metadata"
-        if (graph.getElements().length !== 0) defaultChecked = "Graph"
-        else if (graph.Data.length !== 0) defaultChecked = "Table"
+        let defaultChecked: Tab = "Graph"
+        if (graph.Data.length !== 0) defaultChecked = "Table"
+        if (graph.CurrentQuery && graph.CurrentQuery.metadata.length > 0 && graph.Metadata.length > 0 && graph.CurrentQuery.explain.length > 0) defaultChecked = "Metadata"
 
         setTabsValue(defaultChecked);
     }, [graph, graph.Id, elementsLength, graph.Data.length, isTabEnabled, tabsValue])
