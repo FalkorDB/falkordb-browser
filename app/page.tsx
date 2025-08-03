b9c98dc736ec1fc26d21c28250dd1df18afa5705
Guy Korland <gkorland@gmail.com>
2025-08-03 16:30:14 +0300

    fix logo in light mode

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,21 @@ export default function Home() {
   return (
     <div className="h-full w-full bg-background flex flex-col gap-4">
       <main className="grow flex flex-col gap-10 items-center justify-center">
-      <Image style={{ width: 'auto', height: '100px' }} priority src="/BrowserLogo.svg" alt="FalkorDB Logo" width={0} height={0} />
-      <Spinning />
+        <div className="flex items-center gap-2">
+          <Image 
+            className="invert dark:invert-0 transition-all duration-200" 
+            style={{ width: 'auto', height: '100px' }} 
+            priority src="/FalkorDBLogo.svg" 
+            alt="FalkorDB Logo" width={0} height={0} 
+          />
+          <div className="w-px h-16 bg-border mx-8" />
+          <Image 
+            style={{ width: 'auto', height: '100px' }} 
+            priority src="/BrowserLogo.svg" 
+            alt="Browser Logo" width={0} height={0} 
+          />
+        </div>
+        <Spinning />
       </main>
       <div className="flex flex-col gap-8 justify-center items-center">
         <p className="text-foreground">Version: {`{${pkg.version}}`}</p>
