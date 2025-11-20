eed6274f93d77b50841d62aeb60a4912c9fe8753
Anchel135 <yakoveidelman1@gmail.com>
2025-11-20 12:46:50 +0200

    fix build

--- app/providers.tsx
+++ app/providers.tsx
@@ -615,7 +615,7 @@ function ProvidersWithSession({ children }: { children: React.ReactNode }) {
                               graphNames={pathname.includes("/schema") ? schemaNames : graphNames}
                               onSetGraphName={handleOnSetGraphName}
                               onOpenGraphInfo={onExpand}
-                              displayChat={displayChat}
+                              navigateToSettings={navigateToSettings}
                             />
                           }
                           <ResizablePanelGroup direction="horizontal" className="w-1 grow">
