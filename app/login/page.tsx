0c5a9c4b6eb98a27d0ed3dc187e4f3442453e4f4
Anchel135 <yakoveidelman1@gmail.com>
2025-12-03 13:44:24 +0200

    Refactor LoginForm and Page components for improved layout and structure. Update LoginForm to use 'basis-0' for flex growth and 'max-h-full' for better overflow handling. Remove Suspense wrapper from Page component to simplify rendering.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,9 @@
-import { Suspense } from "react";
 import LoginForm from "./LoginForm";
 
 export default function Page() {
   return (
     <div className="Page">
-      <Suspense>
         <LoginForm />
-      </Suspense>
     </div>
   );
 }
