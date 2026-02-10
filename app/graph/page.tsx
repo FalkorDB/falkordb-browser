06490c5351df16f406958286fefc42cba8f28772
Anchel135 <yakoveidelman1@gmail.com>
2026-02-10 14:13:32 +0200

    fix: update panel state handling in element selection logic

--- app/graph/page.tsx
+++ app/graph/page.tsx
@@ -186,14 +186,23 @@ export default function Page() {
     const handleSetSelectedElements = useCallback((el: (Node | Link)[] = []) => {
         setSelectedElements(el);
 
+        setPanel(prev => {
+            if (el.length !== 0) {
+                return "data";
+            } 
+            
+            if (prev !== "chat") {
+                return undefined;
+            }
+
+            return prev;
+        });
+
         if (el.length !== 0) {
-            setPanel("data");
             setIsAddEdge(false);
             setIsAddNode(false);
-        } else if (panel !== "chat") {
-            setPanel(undefined);
         }
-    }, [panel, setPanel]);
+    }, [setPanel]);
 
     useEffect(() => {
         handleSetSelectedElements();
@@ -234,7 +243,7 @@ export default function Page() {
                 }
             } else {
                 const link = graph.extendEdge(json.result.data[0].e, false, false, true);
-                
+
                 if (link) {
                     setRelationships(prev => [...prev.filter(p => p.name !== link.relationship), graph.RelationshipsMap.get(link.relationship)!]);
                     handleSetIsAdd(setIsAddEdge, setIsAddNode)(false);
@@ -321,6 +330,7 @@ export default function Page() {
                 );
 
             case "data":
+
                 if (selectedElements.length === 0) return undefined;
 
                 return <DataPanel
